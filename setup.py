b65a6ba7eab10370b5ab506c6249091c26343a5c
Deniz Koluaçık <koluacik@disroot.org>
2020-12-14 22:59:42 +0300

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='uppaal-py',
-        version='0.0.1',
+        version='0.0.2',
         description='UPPAAL wrapper for Python',
         packages=find_packages(),
         long_description=open('README.md').read(),
